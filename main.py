eb163d4d2db67ff5337e76e1bf90fee72ca36c2e
Saadi07 <saadiawan09@gmail.com>
2023-12-10 20:28:21 +0500

    added health check

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 app.include_router(event_router)
 
+@app.get("/" ,tags=["health-check"])
+def health_check():
+    return {"message": "Server is up and running"}
 
 # if __name__ == "__main__":
  
